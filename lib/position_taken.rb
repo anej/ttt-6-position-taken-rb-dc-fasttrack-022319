2e6451bc853c591033a5083d05b3b59c7e2e5247
Jena P <jpersico@gmail.com>
2019-06-09 02:05:08 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 # code your #position_taken? method here!
-def position_taken?(index)
-  if index is empty 
-    return true
+def position_taken?(board, index)
+  if board[index] == " " 
+    return false 
+  else
+    return true 
 end
